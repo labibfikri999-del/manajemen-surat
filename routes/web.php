d33c7ef073df709e6a9c2479fe149385d58e44d0
Arsiparis Admin <admin@arsiparis.com>
2025-12-13 21:21:24 +0800

    Add debug-zip route to web.php

--- routes/web.php
+++ routes/web.php
@@ -128,3 +128,19 @@ Route::prefix('api')->middleware('auth')->group(function(){
 
 
 
+
+// Debug Route for Zip
+Route::get('/debug-zip', function () {
+    $disabled_functions = explode(',', ini_get('disable_functions'));
+    return [
+        'PHP_OS' => PHP_OS,
+        'ZipArchive_Class_Exists' => class_exists('ZipArchive'),
+        'shell_exec_Exists' => function_exists('shell_exec'),
+        'shell_exec_Disabled_INI' => in_array('shell_exec', $disabled_functions),
+        'exec_Exists' => function_exists('exec'),
+        'Storage_Path' => storage_path('app/public'),
+        'Path_Exists' => File::exists(storage_path('app/public')),
+        'File_Count' => File::exists(storage_path('app/public')) ? count(File::allFiles(storage_path('app/public'))) : 'Path not found',
+        'Zip_Command_Check' => function_exists('shell_exec') ? shell_exec('zip -v') : 'shell_exec disabled',
+    ];
+});
