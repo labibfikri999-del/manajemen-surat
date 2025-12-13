a1890e271b0dd9075a7b9d2c0871a88e353873ec
Arsiparis Admin <admin@arsiparis.com>
2025-12-13 21:00:09 +0800

    Fix: Restore sidebar label to 'Arsip Digital' while keeping functioning route

--- resources/views/partials/sidebar-menu.blade.php
+++ resources/views/partials/sidebar-menu.blade.php
@@ -49,7 +49,7 @@
             'roles' => ['direktur', 'staff', 'instansi'],
         ],
         [
-            'name' => 'Arsip Dokumen', // Renamed from Arsip Digital
+            'name' => 'Arsip Digital',
             'route' => 'arsip-digital',
             'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"/>',
             'roles' => ['direktur', 'staff'],
